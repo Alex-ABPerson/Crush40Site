7b6a15655d03f2afde6191d73d82d39091207070
Alex-ABWorld <alexbu2015@hotmail.com>
2021-12-17 14:08:27 +0000

    Add source

---
+++ README.md
@@ -0,0 +1,5 @@
+# Crush 40 Website
+
+The official repository for the community-made *Crush 40 website*!
+
+**This README will be expanded in the future**
---
+++ scrap/floating.js
@@ -0,0 +1,20 @@
+/*let floating = document.getElementsByClassName('floating');
+
+window.addEventListener('load', () => {
+    for (let i = 0; i < floating.length; i++)
+    {
+        floating[i].style.marginTop = '0px';
+    }
+});
+
+window.addEventListener('scroll', () => {
+    for (let i = 0; i < floating.length; i++)
+    {
+        console.log(floating[i].style.marginTop);
+        //let currentVal = parseInt(floating[i].style.marginTop.substring(0, floating[i].style.marginTop.length - 2));
+
+        // Move it by the difference in scroll, 
+        floating[i].style.marginBottom = window.scrollY / 3;
+    }
+});
+*/
