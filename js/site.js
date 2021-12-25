0e8838e5ddfbcf5ec2ea1d8a325adacac799d27a
Alex-ABWorld <alexbu2015@hotmail.com>
2021-12-25 23:51:36 +0000

    Add "show panel" JS code

--- js/site.js
+++ js/site.js
@@ -1,18 +1,43 @@
+// Floating Panel
+var floating;
+var floatingPanel;
+var floatingPanelPages;
+var panelButtons;
+
+window.addEventListener('load', () => {
+    
+    floating = document.querySelector(".floating");
+    floatingPanel = document.querySelector(".floating .panel");
+    floatingPanelPages = document.querySelectorAll(".floating .panel .page");
+    panelButtons = document.querySelectorAll("[data-action='panel']");
+
+    for (let btn of panelButtons)
+    {
+        btn.addEventListener('click', () => {
+            floating.style.visibility = 'visible'; // Preventing input presses
+            floatingPanel.style.visibility = 'visible';
+
+            for (let page of floatingPanelPages)
+                page.style.visibility = page.dataset.pageName == btn.dataset.pageName ? 'visible' : 'hidden';
+        });
+    }
+});
+
 // Parallax movement (e.g. guitar separator)
 var backgroundParallax;
 
 window.addEventListener('load', () => {
-
+    
     backgroundParallax = document.querySelectorAll("[data-parallax]");
 
     window.addEventListener('scroll', () => {
         UpdateParallaxAll();
     });
-
+    
     window.addEventListener('resize', () => {
         UpdateParallaxAll();
     });
-
+    
     UpdateParallaxAll();
 });
 
