337479675800ff889c04c2ea738de18f90bb8693
Alex-ABWorld <alexbu2015@hotmail.com>
2022-01-08 19:58:41 +0000

    Add text content to "Releases" section of discography

--- discography.php
+++ discography.php
@@ -23,28 +23,81 @@
         <?php include('base/bodyStart.html') ?>
 
         <!-- Albums -->
-        <div class="albums section" id="intro">
+        <div class="albums-intro section" id="intro">
             <div class="intro">
-                <h2>Albums</h2>
-                <p>Crush 40 has released a number of albums over their time, find out all about them below!</p>
+                <h2>Releases</h2>
+                <p>Crush 40 has released a number of EPs and albums over the years, here's a list of all the dedicated Crush 40 EPs or albums they've released.</p>
             </div>
-            <div class="albumList">
-                <div class="album albumLeft album2000">
-                    <div class="info">
-                        <p>2000</p>
-                        <h3>Thrill of the Feel</h3>
-                        <p>Thrill of the Feel is the first</p>
-                    </div>
-                    <img class="img" src="img/content/discography/ThrillOfTheFeel.jpg">
+        </div>
+
+        <div class="albumList section">
+            <div class="album albumLeft album2000">
+                <div class="info">
+                    <p>2000</p>
+                    <h3>Thrill of the Feel</h3>
+                    <p>This is where it all began, the first album put out by the band themselves. Thrill Of The Feel is comprised of all the songs done with a full band performance featured in the game "EA SPORTS NASCAR ARCADE". Some tracks on this album do not have any vocals, making this album the only album released by the band that features instrumental tracks.</p>
+                    <p>This album also includes "Sonic Adventure"'s Open Your Heart as a bonus track, officially making the song part of the band's works.</p>
                 </div>
-                <div class="album albumRight album2009">
-                    <div class="info">
-                        <p>2009</p>
-                        <h3>Super Sonic Songs</h3>
-                        <p>Hello.</p>
-                    </div>
-                    <img class="img" src="img/content/discography/SuperSonicSongs.png">
+                <img class="img" src="img/content/discography/ThrillOfTheFeel.jpg">
+            </div>
+            <div class="album albumRight album2000">
+                <div class="info">
+                    <p>2003</p>
+                    <h3>Crush 40</h3>
+                    <p>This album marks the first time the name "Crush 40" was used. All of the vocal tracks from Thrill of the Feel are featured on this album, so in theory, this album is just a western release of that album. However, the instrumental tracks are missing and in their place are Live & Learn and two bonus tracks: It Doesn't Matter (with Tony Harnell on vocals), and Escape From The City (with Ted Poley & Tony Harnell on vocals).</p>
+                    <p>The name "Crush 40" would be carried over to the game "Sonic Heroes", which would be released later in 2003.</p>
                 </div>
+                <img class="img" src="img/content/discography/Crush40.jpg">
+            </div>
+            <div class="album albumLeft album2009">
+                <div class="info">
+                    <p>2009</p>
+                    <h3>Super Sonic Songs</h3>
+                    <p>This is the first compilation album released by Crush 40. Comprised of a range of tracks from the band's ~10-year history. Some old tracks were remixed and the following new tracks were featured too: A cover of the vocal theme for "Sonic Riders: Zero Gravity", Un-gravitify, a cover of The Cult's Fire Woman, and Is It You - a fully original song created for this release, which was first original to be created by Crush 40.</p>
+                    <p>While the original CD release of Super Sonic Songs did not feature any bonus tracks, a few years after its release, the album was placed on digital platforms with 3 bonus tracks: Free, With Me -Massive Power Mix-, and Seven Rings In Hand - these songs had been previously featured elsewhere, however.</p>
+                </div>
+                <img class="img" src="img/content/discography/SuperSonicSongs.png">
+            </div>
+            <div class="album albumRight album2012">
+                <div class="info">
+                    <p>2012</p>
+                    <h3>Rise Again</h3>
+                    <p>Throughout 2011 and 2012, Crush 40 released four singles: <i>Rise Again</i>, <i>Sonic Youth</i>, <i>One of Those Days</i> and <i>Song of Hope</i>. This EP is a compilation of these singles released by Crush 40 throughout 2011 and 2012.</p>
+                    <p>There was a variant of this EP available to be purchased at <i>Sonic Boom 2012</i>, that excluded the Song of Hope single from 2011.</p>
+                </div>
+                <img class="img" src="img/content/discography/RiseAgain.jpg">
+                <div class="riseAgainBackground"></div>
+            </div>
+            <div class="albumSlantedSeparator">
+
+            </div>
+            <div class="album albumLeft albumLive">
+                <div class="info">
+                    <p>2012</p>
+                    <h3>Live!</h3>
+                    <p>Live! is Crush 40's very first live album. Crush 40 performed two shows at Live Stage Guilty in Tokyo on March 29th 2012 and March 30th 2012, and both shows were professionally recorded in full for this album. This was the very first time Crush 40 could be heard with a full band live outside of Japan.</p>
+                    <p>All 24 songs performed at these shows were released, with the exception of a cover of Hardline's "Everything", which has not yet been released due to copyright concerns. There were very few official videos of these concerts released. This is in part due to the way the CD was edited, combining the best pieces of both shows, making it very hard to correctly sync up to just the video for one show.</p>
+                </div>
+                <img class="img" src="img/content/discography/LIVE!.png">
+            </div>
+            <div class="album albumRight album2014">
+                <div class="info">
+                    <p>2015</p>
+                    <h3>2 Nights 2 Remember</h3>
+                    <p>2 Nights 2 Remember is both a studio EP and a live album. The first four tracks on the CD are brand-new studio songs created for the EP, while the rest of the CD is professional audio of their <i>2 Nights 2 Remember</i> live performance, essentially "padding" the free space left on the CD.</p>
+                    <p>The 2 Nights 2 Remember live performance was at Live Stage Guilty, on March 29th 2014 and March 30th 2014. Once again, both shows were professionally recorded and put on the CD in exactly the same manner as Live!.</p>
+                    <p>The title of the album, <i>2 Nights 2 Remember</i> is a reference to the two shows Crush 40 performed in 2011 at Live Stage Guilty, and how much they meant to the band.</p>
+                </div>
+                <img class="img" src="img/content/discography/2N2R.png">
+            </div>
+            <div class="album albumLeft album2019">
+                <div class="info">
+                    <p>2019</p>
+                    <h3>Driving Through Forever</h3>
+                    <p>Driving Through Forever is Crush 40's most recent album. It is another compilation album, celebrating the past 20 years of Crush 40. From Open Your Heart, written in 1998, to Green Light Ride, written in 2018.</p>
+                    <p>Like the previous compilation album, Driving Through Forever contains a new mix of His World and a new song. The new being Call Me Crazy, which had only ever been performed live before the album released, at Japan Game Music Festival II:RE.</p>
+                </div>
+                <img class="img" src="img/content/discography/DrivingThroughForever.jpg">
             </div>
         </div>
 
