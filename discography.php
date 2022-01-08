156bf30620a560aff2a3c738d27931082939c5bf
Alex-ABWorld <alexbu2015@hotmail.com>
2022-01-08 13:14:27 +0000

    Create discography page

---
+++ discography.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Crush 40</title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="The biggest fan-made Crush 40 website on the planet!">
+        <meta name="keywords" content="Crush 40, Johnny Gioeli, Jun Senoue, Sonic the Hedgehog, Crush 40 Live, Crush 40 Discography">
+
+        <link href="css/site.css" rel="stylesheet">
+        <link href="css/discography.css" rel="stylesheet">
+
+        <!-- Crush 40 JavaScript -->
+        <script src="js/site.js"></script>
+    </head>
+    <body>
+        <?php include('base/bodyStart.html') ?>
+
+        <!-- Albums -->
+        <div class="albums section" id="intro">
+            <div class="intro">
+                <h2>Albums</h2>
+                <p>Crush 40 has released a number of albums over their time, find out all about them below!</p>
+            </div>
+            <div class="albumList">
+                <div class="album albumLeft album2000">
+                    <div class="info">
+                        <p>2000</p>
+                        <h3>Thrill of the Feel</h3>
+                        <p>Thrill of the Feel is the first</p>
+                    </div>
+                    <img class="img" src="img/content/discography/ThrillOfTheFeel.jpg">
+                </div>
+                <div class="album albumRight album2009">
+                    <div class="info">
+                        <p>2009</p>
+                        <h3>Super Sonic Songs</h3>
+                        <p>Hello.</p>
+                    </div>
+                    <img class="img" src="img/content/discography/SuperSonicSongs.png">
+                </div>
+            </div>
+        </div>
+
+        <?php include('base/bodyEnd.html') ?>
+    </body>
+</html>
