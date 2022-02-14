db80848d4cc61fe24ca7cfd800607e3edbaf82ad
Alex-ABWorld <alexbu2015@hotmail.com>
2022-02-14 15:10:13 +0000

    Start gear-guitars section

--- gear.php
+++ gear.php
@@ -27,11 +27,49 @@
                 <img src="img/logo/FanWebsiteLogo.svg" alt="Crush 40 Logo">
 
                 <!-- Text -->
-                <h2>The gear Crush 40 uses, from guitars, to amps - all right here!</h2>
+                <h2>The gear Crush 40 uses: Guitars, amps, DAWs, hardware - all right here!</h2>
 
                 <div class="btnSet">
                     <a class="btnLink"><button class="filledButton btnAlbums btnWidthNormal">Guitars <img src="img/icons/smallDownArrow.svg" alt="Down Arrow"></button></a>
                     <button class="emptyButton btnViewAll btnWidthNormal">Amplifiers <img src="img/icons/smallDownArrow.svg" alt="Down Arrow"></button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Guitars -->
+        <div class="guitars section">
+            <div class="intro">
+                <h1>Guitars</h1>
+
+                <div class="content">
+                    <div class="text">
+                        <p>By far the most prominent set of gear for Crush 40 is Jun's <i>guitars</i>.</p>
+                        <p>Jun uses custom-made <b>ESP Guitars</b> for his own works and Crush 40. The guitars have different designs on them - some being used for general Sonic/Shadow songs, and some being used for very precise game soundtracks.</p>
+                        <p>Jun most commonly plays the <b>ESP GL "SONIC"</b>, <b>ESP SONIC-II</b> and <b>ESP SHADOW-II</b> at most Crush 40 shows, but at some shows he also brings the dedicated game guitars to play.</p>
+                        <p>He's been in a sponsership deal with ESP ever since he first worked on Sonic Adventure - and as such is required to use these guitars at his shows, but that's not a problem, because they're awesome and he always sounds great anyway!</p>
+                    </div>
+                    <img data-action="zoom" class="img" src="img/content/guitars/AllGuitars.jpg" alt="Some of Jun's guitars">
+                </div>
+            </div>
+            
+            <div class="details">
+                <div class="images">
+                    <div class="imgFrame frame">
+                        <img data-action="zoom" class="img" src="img/content/guitars/AllGuitars2.png" alt="Some of Jun's guitars">
+                        <p>Jun's guitars backstage for the JGMF2013 show.</p>
+                    </div>
+                    <div class="imgFrame frame">
+                        <img data-action="zoom" class="img" src="img/content/guitars/Exhibition.png" alt="Some of Jun's guitars">
+                        <p>An <a href="https://soahcity.com/2016/08/23/jun-senoues-sonic-guitars-on-display-at-esp-museum/">exhibit</a> dedicated to Jun's guitars at the ESP Museum.</p>
+                    </div>
+                    <div class="quoteFrame frame">
+                        <div class="quotation">
+                            <p class="mark quotationStart">"</p>
+                            <p class="text">Jun Senoue has 742 guitars! And this is #82! Whew. Thank goodness you only brought <i>82</i>.</p>
+                            <p class="mark quotationEnd">"</p>
+                        </div>
+                        <p class="text">- Johnny Gioeli, <i>2 Nights 2 Remember</i>, 2014.</p>
+                    </div>
                 </div>
             </div>
         </div>
