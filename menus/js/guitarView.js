c01c8196b8a2b88de52a2ac99b41ef2aa37d5780
Alex-ABWorld <alexbu2015@hotmail.com>
2022-08-16 23:42:21 +0100

    Start guitarview load logic + New Shadow-II images

--- menus/js/guitarView.js
+++ menus/js/guitarView.js
@@ -1,11 +1,15 @@
 let gearPageNav;
+let gtrList;
 
-let Guitars = [
-
-];
+let Guitars = {
+    sonic: {
+        title: "ESP GL 'SONIC'"
+    }
+};
 
 window.addEventListener('load', () => {
     gearPageNav = document.querySelector("#gearPageNav");
+    gtrList = document.querySelector("#guitarList");
 
     window.addEventListener('message', (e) => {
         if (e.data.includes("gear.php"))
@@ -13,11 +17,37 @@ window.addEventListener('load', () => {
     });
 
     let afterG = document.URL.split('?')[1].substring(2); // Trim off the "g="
+    if (afterG != "") SelectGtrByID(gtr);
+
+    PopulateGtrList();
 
     window.parent.postMessage('!700px', '*');
 });
 
-function Populate()
+function PopulateGtrList()
+{
+    gtrList.replaceChildren();
+
+    for (let gtr of Object.keys(Guitars))
+    {
+        let newItm = document.createElement("li");
+        newItm.addEventListener('click', () => Populate(Guitars[gtr]));
+        newItm.dataset.gtr = gtr; // Used for opening to a specific guitar
+
+        let newItmPara = document.createElement("p");
+        newItmPara.innerHTML = Guitars[gtr].title;
+
+        newItm.appendChild(newItmPara);
+        gtrList.appendChild(newItm);
+    }
+}
+
+function SelectGtrByID(gtr)
+{
+    // Find the item in the guitar list
+}
+
+function Populate(gtr)
 {
     
 }
